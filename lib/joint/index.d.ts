8bad5c2f03abc7bfc8aff9df1d87ba20d49113ed
Oliver Zell <zOadT@users.noreply.github.com>
2020-07-20 19:45:14 +0200

    Revert "fix serialization"
    This reverts commit 02b75a69948b8f599b0cecb4479b7354d9b4aba4.

--- lib/joint/index.d.ts
+++ lib/joint/index.d.ts
@@ -506,7 +506,6 @@ export type WeldJointOpt = JointOpt & Partial<{
 export type WeldJointDef = JointDef & WeldJointOpt & {
   localAnchorA: Vec2,
   localAnchorB: Vec2,
-  localAxisA: Vec2,
 };
 
 export interface WheelJoint extends Joint {
